6c32808a7198ead75ce4d925381a6ac219123d04
OctavianFarcas <octavian.farcas98@gmail.com>
2023-01-30 18:22:18 +0200

    first commit - setting up the repository

---
+++ brain.js
@@ -0,0 +1,7 @@
+'use strict';
+// Selecting elements
+const nikeDropdown = document.getElementById('nikedropdown');
+const newM = document.querySelector('.new');
+console.log(nikeDropdown, newM);
+
+nikeDropdown.classList.add('hidden');
